1659b28cb276bd35f72c6187213fe63df141ffb4
Karl-Haupt <karlhaupt1@gmail.com>
2020-12-11 17:47:05 +0200

    Solve the CSS problem with the Checkout page. The Checkoutpage and CheckoutProuduct page is completed. Tomorrow We got to start with FIrebase

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal.js';
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
+    
     return (
-        <div className="çheckout">
+        <div className="checkout">
             <div className="checkout__left">
-                <img className="checkout__ad" 
-                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
-                     alt="" />
+                <img 
+                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
+                alt="" className="checkout__ad"/>
 
                 <div>
-                    <h2 className="checkout__title">Your Shopping Market</h2>
-                </div>
+                    <h2 className="checkout__title">
+                        Your shopping Basket
+                    </h2>
+
+                    {/* CheckoutProduct */}
+                    {basket.map(item => (
+                        <CheckoutProduct 
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                        />
+                    ))}
+
+                    </div>
             </div>
 
             <div className="checkout__right">
--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import "./Subtotal.css";
-import CurrencyFormat from "react-currency-format";
+import React from 'react'
+import CurrencyFormat from 'react-currency-format';
+import './Subtotal.css';
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './Reducer';
 
 function Subtotal() {
+    const [ {basket}, dispatch] = useStateValue();
+    
     return (
-        <div className="subtotal">
-            <CurrencyFormat renderText= {(value) => {
-                <>
-                  <p>Subtotal (0 items): <strong>0</strong></p>
-                  <small className="subtotal__gift">
-                      <input type="checkbox" />This order contains a gift
-                  </small>
-                </>
-            }} 
-                decimalScale={2} //2 decimals
-                value={0}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"$"}
-            />
-            
-            <button> Proceed to Checkout </button>
-        </div>
-    )
+            <div className="subtotal">
+                <CurrencyFormat renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal ({basket.length} items) : 
+                <strong>{value}</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" name="" id=""/> This order contains a gift
+                        </small>
+                    </>
+                    )}
+                    decimalScale={2}
+                    value={getBasketTotal(basket)}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"$"}
+                />
+                <button>Proceed to Checkout</button>
+            </div>
+        )
 }
+
 
 export default Subtotal;
